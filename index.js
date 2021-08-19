356dc58ea1cf9907fc1d0e30930761f04f0dac0e
Austin Rhoads <austinrrhoads@ymail.com>
2021-08-19 11:58:14 -0500

    commit tree 22ea5b9f593579b6b4e4432b46c38ee341b01de8

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+
+
 function findMinAndRemove(array){
+    let min = array[0];
+    let minIndex = 0;
+
+    for(let x = 0; x < array.length; x++){
+        if(array[x] < min){
+            min = array[x];
+            minIndex = x;
+        }
+    }
+
+    array.splice(minIndex, 1);
+    return min;
 
 }
+
 
 function selectionSort(array){
+    let min;
+    let sorted = [];
+
+    while(array.length != 0){
+        min = findMinAndRemove(array);
+        sorted.push(min)
+    }
+    return sorted;
 
 }
+
+
+
+var testArray1 = [3, 8, 4, 6, 7, 11, 4, 1]
+
+console.log(selectionSort(testArray1))
+//console.log( findMinAndRemove(testArray1))
